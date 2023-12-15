4d0e289930745d6d368bfc044043c7f62711dc12
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2023-12-15 13:03:14 +0000

    Fixing some minor issues in help pages

--- R/summary.metarep.R
+++ R/summary.metarep.R
@@ -5,10 +5,10 @@
 #' @param  object An object of class 'metarep'.
 #' @param ... Arguments to be passed to methods, see \code{summary.meta}
 #' @return A list of the quantities for replicability analysis, as follows: 
-#'   \item{meta-analysis results: } {Summary of the supplied 'meta' object.}
-#'   \item{r.value: } {r-value of the tested alternative.}
-#'   \item{u.increased: } {Maximal number of studies at which replicability of increasing effect can be claimed. It will be reported unless the alternative is 'less'.}
-#'   \item{u.decreased: } {Maximal number of studies at which replicability of increasing effect can be claimed. It will be reported unless the alternative is 'greater'.}
+#'   \item{meta-analysis results: }{Summary of the supplied 'meta' object.}
+#'   \item{r.value: }{r-value of the tested alternative.}
+#'   \item{u.increased: }{Maximal number of studies at which replicability of increasing effect can be claimed. It will be reported unless the alternative is 'less'.}
+#'   \item{u.decreased: }{Maximal number of studies at which replicability of increasing effect can be claimed. It will be reported unless the alternative is 'greater'.}
 #' 
 #' @export
 #'
